918998f3476ba0ad9fefe56e21034c894003cfb9
Vincent <vincent.primault@gmail.com>
2024-06-07 11:52:31 +0200

    test: add unit tests in ui and storage packages

---
+++ packages/storage/src/db.test.ts
@@ -0,0 +1,26 @@
+import { test, expect } from "@jest/globals";
+import { db } from "./db";
+
+test("db should save a connection", async () => {
+    const connection = { id: "", label: "Connection", baseUrl: "https://api.github.com", host: "github.com", auth: "ghp_xxx" };
+    const id = await db.connections.add(connection);
+    await expect(db.connections.get(id)).resolves.toEqual(connection);
+});
+
+test("db should save a section", async () => {
+    const section = {
+        id: "",
+        label: "label",
+        search: "author:@me",
+        notified: false,
+        position: 0,
+    };
+    const id = await db.sections.add(section);
+    await expect(db.sections.get(id)).resolves.toEqual(section);
+});
+
+test("db should save a star", async () => {
+    const star = { uid: "github.com,pvcnt/reviewer,1" };
+    const id = await db.stars.add(star);
+    await expect(db.stars.get(id)).resolves.toEqual(star);
+});
---
+++ packages/ui/src/components/Footer.test.tsx
@@ -0,0 +1,12 @@
+/**
+ * @jest-environment jsdom
+ */
+import { test, expect } from "@jest/globals";
+import { render } from "@testing-library/react";
+import Footer from "./Footer";
+
+test("Footer renders a link to the commit", () => {
+    const { getByRole } = render(<Footer commit="abcdef"/>);
+    const link = getByRole("link", { name: "abcdef" });
+    expect(link.getAttribute("href")).toBe("https://github.com/pvcnt/reviewer/commit/abcdef");
+});
