bb61d336a2fb9e6728c0f285a215be9387be688a
Vincent <vincent.primault@gmail.com>
2024-06-16 14:59:51 +0200

    fix: better capture clicks on rows

--- packages/ui/src/components/PullTable.tsx
+++ packages/ui/src/components/PullTable.tsx
@@ -37,57 +37,47 @@ export default function PullTable({pulls, isStarred, onStar}: Props) {
             </thead>
             <tbody>
                 {pulls.map((pull, idx) => (
-                    <tr key={idx}>
-                        <td className="cursor-pointer" onClick={() => onStar(pull)}>
+                    <tr key={idx} onClick={() => window.location.href = pull.url}>
+                        <td className="cursor-pointer" onClick={(e) => { onStar(pull); e.stopPropagation(); }}>
                             {isStarred(pull)
                                 ? <IconWithTooltip icon="star" color="#FBD065" title="Unstar pull request"/>
                                 : <IconWithTooltip icon="star-empty" title="Star pull request"/>}
                         </td>
                         <td>
-                            <a href={pull.url}>
-                                <div className="pull-author">
-                                    <Tooltip content={pull.author.name}>
-                                        {pull.author.avatarUrl ? <img src={pull.author.avatarUrl}/> : <Icon icon="user"/>}
-                                    </Tooltip>
-                                </div>
-                            </a>
-                        </td>
-                        <td>
-                            <a href={pull.url}>
-                                {pull.state == PullState.Draft
-                                ? <IconWithTooltip icon="document" title="Draft" color="#5F6B7C"/>
-                                : pull.state == PullState.Merged
-                                ? <IconWithTooltip icon="git-merge" title="Merged" color="#634DBF"/>
-                                : pull.state == PullState.Closed
-                                ? <IconWithTooltip icon="cross-circle" title="Closed" color="#AC2F33"/>
-                                : pull.state == PullState.Approved
-                                ? <IconWithTooltip icon="git-pull" title="Approved" color="#1C6E42"/>
-                                : pull.state == PullState.ChangesRequested
-                                ? <IconWithTooltip icon="issue" title="Changes requested" color="#C87619"/>
-                                : pull.state == PullState.Pending
-                                ? <IconWithTooltip icon="git-pull" title="Pending review" color="#C87619"/>
-                                : null}
-                            </a>
-                        </td>
-                        <td>
-                            <a href={pull.url}>
-                                <Tooltip content={formatDate(pull.updatedAt)}>
-                                    <ReactTimeAgo date={new Date(pull.updatedAt)} tooltip={false} timeStyle="round"/>
+                            <div className="pull-author">
+                                <Tooltip content={pull.author.name}>
+                                    {pull.author.avatarUrl ? <img src={pull.author.avatarUrl}/> : <Icon icon="user"/>}
                                 </Tooltip>
-                            </a>
+                            </div>
                         </td>
                         <td>
-                            <a href={pull.url}>
-                                <Tooltip content={<><span className="additions">+{pull.additions}</span> / <span className="deletions">-{pull.deletions}</span></>} openOnTargetFocus={false} usePortal={false}><Tag>{computeSize(pull)}</Tag></Tooltip>
-                            </a>
+                            {pull.state == PullState.Draft
+                            ? <IconWithTooltip icon="document" title="Draft" color="#5F6B7C"/>
+                            : pull.state == PullState.Merged
+                            ? <IconWithTooltip icon="git-merge" title="Merged" color="#634DBF"/>
+                            : pull.state == PullState.Closed
+                            ? <IconWithTooltip icon="cross-circle" title="Closed" color="#AC2F33"/>
+                            : pull.state == PullState.Approved
+                            ? <IconWithTooltip icon="git-pull" title="Approved" color="#1C6E42"/>
+                            : pull.state == PullState.ChangesRequested
+                            ? <IconWithTooltip icon="issue" title="Changes requested" color="#C87619"/>
+                            : pull.state == PullState.Pending
+                            ? <IconWithTooltip icon="git-pull" title="Pending review" color="#C87619"/>
+                            : null}
                         </td>
                         <td>
-                            <a href={pull.url}>
-                                <div className="font-semibold">{pull.title}</div>
-                                <div className="text-sm">
-                                    {pull.host}:{pull.repository} #{pull.number}
-                                </div>
-                            </a>
+                            <Tooltip content={formatDate(pull.updatedAt)}>
+                                <ReactTimeAgo date={new Date(pull.updatedAt)} tooltip={false} timeStyle="round"/>
+                            </Tooltip>
+                        </td>
+                        <td>
+                            <Tooltip content={<><span className="additions">+{pull.additions}</span> / <span className="deletions">-{pull.deletions}</span></>} openOnTargetFocus={false} usePortal={false}><Tag>{computeSize(pull)}</Tag></Tooltip>
+                        </td>
+                        <td>
+                            <div className="font-semibold">{pull.title}</div>
+                            <div className="text-sm">
+                                {pull.host}:{pull.repository} #{pull.number}
+                            </div>
                         </td>
                     </tr>
                 ))}
