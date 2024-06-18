088664bc5e769a4118e642a5d1ddec10f90a405d
Vincent Primault <vincent.primault@gmail.com>
2024-06-18 18:08:49 +0200

    rename project to Mergeable (#47)

--- packages/ui/src/components/Footer.tsx
+++ packages/ui/src/components/Footer.tsx
@@ -5,8 +5,8 @@ type Props = {
 export default function Footer({commit}: Props) {
     return (
         <footer>
-            Reviewer @ {commit 
-                ? <a href={`https://github.com/pvcnt/reviewer/commit/${commit}`}>{commit.substring(0, 7)}</a> 
+            Mergeable @ {commit 
+                ? <a href={`https://github.com/pvcnt/mergeable/commit/${commit}`}>{commit.substring(0, 7)}</a> 
                 : 'devel'}
         </footer>
     )
