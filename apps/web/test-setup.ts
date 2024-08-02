f478b2589adb072c24bee0d2b29c51b888c3b0a1
Vincent <vincent.primault@gmail.com>
2024-08-02 22:47:40 +0200

    fix

--- apps/web/test-setup.ts
+++ apps/web/test-setup.ts
@@ -4,7 +4,3 @@ import "@testing-library/jest-dom/vitest";
 
 // Auto-cleanup after each test.
 afterEach(() => cleanup());
-
-// structuredClone is missing in jsdom environment.
-// https://stackoverflow.com/a/76729230
-global.structuredClone = v => JSON.parse(JSON.stringify(v));
