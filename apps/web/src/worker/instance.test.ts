76e5f0fd1e4857b7daa473e114020364454cb1c7
Vincent <vincent.primault@gmail.com>
2024-08-12 16:03:46 +0200

    test: add tests for worker

---
+++ apps/web/src/worker/instance.test.ts
@@ -0,0 +1,118 @@
+import { TestGitHubClient } from "@repo/github";
+import { afterEach, beforeEach, it, describe, expect, assert } from "vitest";
+import { syncPullsOnce, syncViewersOnce } from "./instance";
+import { db } from "@repo/storage";
+import { mockPull, mockConnection, mockSection } from "@repo/testing";
+
+describe("sync viewers", () => {
+    let client: TestGitHubClient|undefined;
+
+    beforeEach(async () => {
+        client = new TestGitHubClient();
+        await db.connections.add(mockConnection({ id: "1" }));
+        await db.connections.add(mockConnection({ id: "2" }));
+    })
+
+    afterEach(async () => {
+        await db.activities.clear();
+        await db.connections.clear();
+    })
+
+    it("should update connections", async () => {
+        assert(client !== undefined);
+        await syncViewersOnce(client);
+
+        let connection = await db.connections.get("1");
+        expect(connection).toBeDefined();
+        expect(connection?.viewer?.user).toEqual({ name: "test[1]", avatarUrl: "" });
+
+        connection = await db.connections.get("2");
+        expect(connection?.viewer).toBeDefined();
+        expect(connection?.viewer?.user).toEqual({ name: "test[2]", avatarUrl: "" });
+    })
+
+    it("should update activity", async () => {
+        assert(client !== undefined);
+
+        let activity = await db.activities.get("syncViewers");
+        expect(activity).toBeUndefined();
+
+        await syncViewersOnce(client);
+
+        activity = await db.activities.get("syncViewers");
+        expect(activity?.running).toBeFalsy();
+        expect(activity?.refreshTime.getTime()).toBeGreaterThan(0);
+        expect(activity?.refreshTime.getTime()).toBeLessThanOrEqual(Date.now());
+    })
+})
+
+describe("sync pulls", () => {
+    let client: TestGitHubClient|undefined;
+
+    beforeEach(async () => {
+        client = new TestGitHubClient();
+        await db.connections.add(mockConnection({ id: "1" }));
+        await db.connections.add(mockConnection({ id: "2" }));
+        await db.sections.add(mockSection({ id: "1", search: "author:@me draft:true" }));
+        await db.sections.add(mockSection({ id: "2", search: "author:@me review:approved" }));
+    })
+
+    afterEach(async () => {
+        await db.activities.clear();
+        await db.connections.clear();
+        await db.sections.clear();
+        await db.pulls.clear();
+    })
+
+    it("should update pulls", async () => {
+        assert(client !== undefined);
+
+        let connection = await db.connections.get("1");
+        assert(connection !== undefined);
+        client.setPulls(connection, "author:@me draft:true", [
+            mockPull({ uid: "1:1" }),
+            mockPull({ uid: "1:2" }),
+        ]);
+        client.setPulls(connection, "author:@me review:approved", [
+            mockPull({ uid: "1:1" }),
+            mockPull({ uid: "1:3" }),
+        ]);
+
+        connection = await db.connections.get("2");
+        assert(connection !== undefined);
+        client.setPulls(connection, "author:@me draft:true", [
+            mockPull({ uid: "2:1" }),
+        ]);
+        client.setPulls(connection, "author:@me review:approved", [
+            mockPull({ uid: "2:2" }),
+        ]);
+
+        await syncPullsOnce(client);
+
+        const pks = await db.pulls.toCollection().primaryKeys();
+        expect(pks.sort()).toEqual(["1:1", "1:2", "1:3", "2:1", "2:2"]);
+
+        let pull = await db.pulls.get("1:1");
+        expect(pull?.sections).toEqual(["1", "2"]);
+
+        pull = await db.pulls.get("1:2");
+        expect(pull?.sections).toEqual(["1"]);
+
+        pull = await db.pulls.get("1:3");
+        expect(pull?.sections).toEqual(["2"]);
+    })
+
+    it("should update activity", async () => {
+        assert(client !== undefined);
+
+        let activity = await db.activities.get("syncPulls");
+        expect(activity).toBeUndefined();
+
+        await syncPullsOnce(client);
+
+        activity = await db.activities.get("syncPulls");
+        expect(activity?.running).toBeFalsy();
+        expect(activity?.refreshTime.getTime()).toBeGreaterThan(0);
+        expect(activity?.refreshTime.getTime()).toBeLessThanOrEqual(Date.now());
+    })
+})
--- packages/storage/src/db.test.ts
+++ packages/storage/src/db.test.ts
@@ -1,32 +1,22 @@
 import { test, expect } from "vitest";
 import { Activity, db } from "./db";
-import { Connection, Pull, PullState, Star } from "@repo/types";
+import { Star } from "@repo/types";
+import { mockConnection, mockSection, mockPull } from "@repo/testing";
 
 test("db should save a connection", async () => {
-    const connection: Connection = {
-        id: "",
-        label: "Connection",
-        baseUrl: "https://api.github.com",
-        host: "github.com",
-        auth: "ghp_xxx",
+    const connection = mockConnection({
         viewer: {
-            user: { uid: "1:1", name: "pvcnt", avatarUrl: "" },
-            teams: [{ uid: "1:1", name: "test" }],
+            user: { name: "pvcnt", avatarUrl: "" },
+            teams: [{ name: "test" }],
         },
         orgs: ["apache", "kubernetes"],
-    };
+    });
     const id = await db.connections.add(connection);
     await expect(db.connections.get(id)).resolves.toEqual(connection);
 });
 
 test("db should save a section", async () => {
-    const section = {
-        id: "",
-        label: "label",
-        search: "author:@me",
-        notified: false,
-        position: 0,
-    };
+    const section = mockSection();
     const id = await db.sections.add(section);
     await expect(db.sections.get(id)).resolves.toEqual(section);
 });
@@ -38,23 +28,7 @@ test("db should save a star", async () => {
 });
 
 test("db should save a pull", async () => {
-    const pull: Pull = {
-        uid: "1:1",
-        host: "github.com",
-        repo: "pvcnt/mergeable",
-        number: 1,
-        title: "Title",
-        state: PullState.Approved,
-        createdAt: "now",
-        updatedAt: "now",
-        url: "https://github.com/pvcnt/mergeable/1",
-        additions: 0,
-        deletions: 0,
-        author: { uid: "1:1", name: "pvcnt", avatarUrl: "" },
-        comments: 0,
-        starred: 0,
-        sections: ["1", "2"],
-    };
+    const pull = mockPull({sections: ["1", "2"]});
     const id = await db.pulls.add(pull);
     await expect(db.pulls.get(id)).resolves.toEqual(pull);
 });
