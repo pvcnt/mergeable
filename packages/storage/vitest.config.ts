0dedac238dceccf260ec49edf382eb83e10637eb
Vincent <vincent.primault@gmail.com>
2024-08-19 20:35:51 +0200

    Add code coverage
    #41

--- packages/storage/vitest.config.ts
+++ packages/storage/vitest.config.ts
@@ -3,5 +3,11 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     setupFiles: ["fake-indexeddb/auto"],
+    coverage: {
+      enabled: true,
+      include: ["src/**"],
+      provider: "istanbul",
+      reporter: ["text-summary", "html", "lcovonly"],
+    },
   },
 });
