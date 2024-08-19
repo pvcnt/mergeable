7a6fbf1d3a043f37cb75b3fde993b28a1b81044e
Vincent <vincent.primault@gmail.com>
2024-08-19 20:36:45 +0200

    reports

--- packages/github/vitest.config.ts
+++ packages/github/vitest.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
       enabled: true,
       include: ["src/**"],
       provider: "istanbul",
-      reporter: ["text-summary", "html", "lcovonly"],
+      reporter: ["text", "lcovonly"],
     },
   },
 });
