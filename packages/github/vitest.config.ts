3da1a689ef861d25240278228dbeab4c5b593816
Vincent <vincent.primault@gmail.com>
2024-08-22 15:16:56 +0200

    include

--- packages/github/vitest.config.ts
+++ packages/github/vitest.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
+    include: ["tests/**"],
     coverage: {
       include: ["src/**"],
       provider: "istanbul",
