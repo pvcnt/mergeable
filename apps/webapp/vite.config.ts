76238b9007633e3d9f016da237fe16f1eb8cb842
Vincent <vincent.primault@gmail.com>
2024-08-23 16:02:50 +0200

    build: add codecov Vite plugin

--- apps/webapp/vite.config.ts
+++ apps/webapp/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { comlink } from "vite-plugin-comlink";
+import { codecovVitePlugin } from "@codecov/vite-plugin";
 import processEnv from "@repo/vite-plugin-process-env";
 
 // https://vitejs.dev/config/
@@ -13,6 +14,11 @@ export default defineConfig({
         "MERGEABLE_GITHUB_URLS",
         "MERGEABLE_PR_SIZES",
       ],
+    }),
+    codecovVitePlugin({
+      enableBundleAnalysis: process.env.CODECOV_TOKEN !== undefined,
+      bundleName: "webapp",
+      uploadToken: process.env.CODECOV_TOKEN,
     }),
   ],
   worker: {
