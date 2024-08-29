ad936eaf4f8d70f724476c408fc64f177c4ef21b
Vincent Primault <vincent.primault@gmail.com>
2024-08-29 16:00:09 +0200

    feat: send telemetry (disabled for now) (#100)
    Ref #66

--- apps/webapp/src/vite-env.d.ts
+++ apps/webapp/src/vite-env.d.ts
@@ -3,6 +3,7 @@
 
 interface ImportMetaEnv {
     readonly VITE_COMMIT_SHA?: string
+    readonly MERGEABLE_NO_TELEMETRY: string
     readonly MERGEABLE_GITHUB_URLS: string
     readonly MERGEABLE_PR_SIZES: string
 }
