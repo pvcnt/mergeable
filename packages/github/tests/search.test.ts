ddaf60544149967b25070aa8d606d4bcceaada8a
Vincent Primault <vincent.primault@gmail.com>
2024-09-06 16:36:50 +0200

    fix: splitting queries incorrectly removed quotes (#109)
    This made it impossible to use any search queries with quotes in it.
    
    Closes #107

--- packages/github/tests/search.test.ts
+++ packages/github/tests/search.test.ts
@@ -128,7 +128,8 @@ test("split a search string into queries", () => {
   expect(splitQueries("a")).toEqual(["a"]);
   expect(splitQueries("a;b")).toEqual(["a", "b"]);
   expect(splitQueries("   a ;  b ")).toEqual(["a", "b"]);
-  expect(splitQueries('"a;b";c')).toEqual(["a;b", "c"]);
+  expect(splitQueries(`"a"`)).toEqual([`"a"`]);
+  expect(splitQueries(`"a;b";c`)).toEqual([`"a;b"`, "c"]);
   expect(splitQueries("a; ;c")).toEqual(["a", "c"]);
 });
 
