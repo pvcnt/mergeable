125665a526875a2044133e5b03b7767963f5cc1f
Vincent Primault <vincent.primault@gmail.com>
2025-05-02 13:58:55 +0200

    test: fix tests that were failing in the 'github' package (#124)

--- packages/github/tests/client.test.ts
+++ packages/github/tests/client.test.ts
@@ -6,9 +6,11 @@ import { CheckState, PullState } from "@repo/model";
 
 useRecording();
 
+const auth = "ghp_token";
+
 test("should return viewer", async () => {
   const client = new DefaultGitHubClient();
-  const connection = mockConnection({ auth: "ghp_token" });
+  const connection = mockConnection({ auth });
 
   const viewer = await client.getViewer(connection);
 
@@ -24,11 +26,19 @@ test("should return viewer", async () => {
 
 test("should search pulls", async () => {
   const client = new DefaultGitHubClient();
-  const connection = mockConnection({ auth: "ghp_token" });
+  const connection = mockConnection({ auth });
 
   const pulls = await client.searchPulls(connection, "repo:pvcnt/sandbox");
 
   expect(pulls).toEqual([
+    {
+      id: "PR_kwDOMoYZC86UfoU-",
+      updatedAt: new Date("2025-04-30T19:17:30.000Z"),
+    },
+    {
+      id: "PR_kwDOMoYZC86UYQCq",
+      updatedAt: new Date("2025-04-29T16:24:02.000Z"),
+    },
     {
       id: "PR_kwDOMoYZC855afun",
       updatedAt: new Date("2024-08-27T13:45:07.000Z"),
@@ -38,47 +48,40 @@ test("should search pulls", async () => {
 
 test("should get pull", async () => {
   const client = new DefaultGitHubClient();
-  const connection = mockConnection({ auth: "ghp_token" });
+  const connection = mockConnection({ auth });
 
-  const pull = await client.getPull(connection, "PR_kwDOFFj3yM53-IjI");
+  const pull = await client.getPull(connection, "PR_kwDOKCpCz85keYan");
 
   expect(pull).toEqual({
-    id: "PR_kwDOFFj3yM53-IjI",
-    repo: "apache/solr",
-    number: 2632,
-    title: "remove deprecated (DocValues,Norms)FieldExistsQuery use",
-    state: PullState.Pending,
-    ciState: CheckState.Success,
-    createdAt: new Date("2024-08-09T17:18:47.000Z"),
-    updatedAt: new Date("2024-08-23T19:12:59.000Z"),
-    url: "https://github.com/apache/solr/pull/2632",
-    additions: 25,
-    deletions: 29,
+    id: "PR_kwDOKCpCz85keYan",
+    repo: "pvcnt/mergeable",
+    number: 13,
+    title: "fix: Handle multiple connections correctly on dashboard",
+    state: PullState.Merged,
+    ciState: CheckState.None,
+    createdAt: new Date("2024-01-18T23:00:38.000Z"),
+    updatedAt: new Date("2024-01-21T05:05:16.000Z"),
+    url: "https://github.com/pvcnt/mergeable/pull/13",
+    additions: 2,
+    deletions: 2,
     author: {
-      name: "cpoerschke",
+      name: "glossawy",
       avatarUrl:
-        "https://avatars.githubusercontent.com/u/6458642?u=8ccfc26ce7209695b2fcca628a59baca42cff00f&v=4",
+        "https://avatars.githubusercontent.com/u/4606234?u=792175a6c93f239c4a8c7c0ddec008e80b9abd0d&v=4",
       bot: false,
     },
-    requestedReviewers: [
-      {
-        name: "HoustonPutman",
-        avatarUrl:
-          "https://avatars.githubusercontent.com/u/3376422?u=323311c61ac2b04e5deac436faaa4f4309fe7beb&v=4",
-        bot: false,
-      },
-    ],
+    requestedReviewers: [],
     requestedTeams: [],
     reviews: [
       {
         author: {
-          name: "dsmiley",
+          name: "pvcnt",
           avatarUrl:
-            "https://avatars.githubusercontent.com/u/377295?u=85f6ade89e5b34f001267475e806da2a52b2755a&v=4",
+            "https://avatars.githubusercontent.com/u/944506?u=d5c9f112310265a0c7b3be509ecc911620eca4ed&v=4",
           bot: false,
         },
-        createdAt: new Date("2024-08-23T19:07:24.000Z"),
-        lgtm: false,
+        createdAt: new Date("2024-01-19T21:17:30.000Z"),
+        lgtm: true,
       },
     ],
     discussions: [
@@ -86,15 +89,48 @@ test("should get pull", async () => {
         resolved: false,
         comments: [
           {
-            id: "PRR_kwDOFFj3yM6GlVVP",
+            id: "IC_kwDOKCpCz85xTyIc",
             author: {
-              name: "dsmiley",
+              name: "pvcnt",
               avatarUrl:
-                "https://avatars.githubusercontent.com/u/377295?u=85f6ade89e5b34f001267475e806da2a52b2755a&v=4",
+                "https://avatars.githubusercontent.com/u/944506?u=d5c9f112310265a0c7b3be509ecc911620eca4ed&v=4",
               bot: false,
             },
-            body: "As there is now FieldExistsQuery covering a range of cases (not just even docValues & norms), this probably obsoletes complexity inside FieldType.getExistenceQuery.  Can we just call that and remove getSpecializedExistenceQuery as needless in lieu of subtypes overriding getExistenceQuery?\r\n\r\nCC @HoustonPutman as you worked on this method",
-            createdAt: new Date("2024-08-23T19:07:24.000Z"),
+            body: "I had not considered this use case. Out of curiosity, how do you use multiple connections to the same host? How would they be configured differently?",
+            createdAt: new Date("2024-01-19T19:46:48.000Z"),
+          },
+          {
+            id: "IC_kwDOKCpCz85xT7ZD",
+            author: {
+              name: "glossawy",
+              avatarUrl:
+                "https://avatars.githubusercontent.com/u/4606234?u=792175a6c93f239c4a8c7c0ddec008e80b9abd0d&v=4",
+              bot: false,
+            },
+            body: "I provide an example in #12 but with github finer-grained tokens you grant access to a specific user/org, giving access to a user does not mean it will show all the repositories they have access to in all their orgs. So I had to create two different tokens one for the org and one for myself.\r\n\r\nBasically the only difference is the token used for the connection.",
+            createdAt: new Date("2024-01-19T20:16:27.000Z"),
+          },
+          {
+            author: {
+              avatarUrl:
+                "https://avatars.githubusercontent.com/u/944506?u=d5c9f112310265a0c7b3be509ecc911620eca4ed&v=4",
+              bot: false,
+              name: "pvcnt",
+            },
+            body: "That makes sense!",
+            createdAt: new Date("2024-01-19T21:14:34.000Z"),
+            id: "IC_kwDOKCpCz85xUNln",
+          },
+          {
+            author: {
+              avatarUrl:
+                "https://avatars.githubusercontent.com/u/944506?u=d5c9f112310265a0c7b3be509ecc911620eca4ed&v=4",
+              bot: false,
+              name: "pvcnt",
+            },
+            body: "Let's move forward with this for now. It's not optimal but I cannot think of anything better at the moment. And as you say, it's all stored in the browser right now - which is an explicit goal of this project (it should work all in the browser).",
+            createdAt: new Date("2024-01-19T21:17:30.000Z"),
+            id: "PRR_kwDOKCpCz85tTMPW",
           },
         ],
       },
@@ -102,19 +138,19 @@ test("should get pull", async () => {
         resolved: false,
         comments: [
           {
-            id: "PRRC_kwDOFFj3yM5nFK8J",
+            id: "PRRC_kwDOKCpCz85XApfm",
             author: {
-              name: "dsmiley",
+              name: "pvcnt",
               avatarUrl:
-                "https://avatars.githubusercontent.com/u/377295?u=85f6ade89e5b34f001267475e806da2a52b2755a&v=4",
+                "https://avatars.githubusercontent.com/u/944506?u=d5c9f112310265a0c7b3be509ecc911620eca4ed&v=4",
               bot: false,
             },
-            body: "I suspect this code pre-dated FieldType.getExistenceQuery -- just call that.",
-            createdAt: new Date("2024-08-23T19:07:24.000Z"),
+            body: "I was considering as an alternative using the connection name that you introduce in #12. But at the same time using the token is a better representation of the identity of the token (e.g., if we switch the names of two connections we should not reuse the cache of the other one).",
+            createdAt: new Date("2024-01-19T21:17:30.000Z"),
           },
         ],
         file: {
-          path: "solr/core/src/java/org/apache/solr/search/facet/MissingAgg.java",
+          path: "src/routes/dashboard.tsx",
         },
       },
     ],
@@ -123,7 +159,7 @@ test("should get pull", async () => {
 
 test("should error when pull does not exist", async () => {
   const client = new DefaultGitHubClient();
-  const connection = mockConnection({ auth: "ghp_token" });
+  const connection = mockConnection({ auth });
 
   await expect(client.getPull(connection, "PR_none")).rejects.toThrowError();
 });
