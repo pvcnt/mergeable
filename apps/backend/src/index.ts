753e142dc2ac9b58ae8d9b35e8327de7bdee6f56
Vincent <vincent.primault@gmail.com>
2025-05-02 10:30:00 +0200

    format

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -2,7 +2,7 @@ import { githubAuth } from "@hono/oauth-providers/github";
 import { Hono } from "hono";
 import { env } from "hono/adapter";
 
-export type Env = {
+type Env = {
   MERGEABLE_GITHUB_CLIENT_ID: string;
   MERGEABLE_GITHUB_CLIENT_SECRET: string;
 };
@@ -12,13 +12,12 @@ const app = new Hono();
 app.get(
   "/auth/github",
   (c, next) => {
-    const { MERGEABLE_GITHUB_CLIENT_ID, MERGEABLE_GITHUB_CLIENT_SECRET } =
-      env<Env>(c);
+    const config = env<Env>(c);
     const middleware = githubAuth({
       scope: ["user", "repo", "read:org"],
       oauthApp: true,
-      client_id: MERGEABLE_GITHUB_CLIENT_ID,
-      client_secret: MERGEABLE_GITHUB_CLIENT_SECRET,
+      client_id: config.MERGEABLE_GITHUB_CLIENT_ID,
+      client_secret: config.MERGEABLE_GITHUB_CLIENT_SECRET,
     });
     return middleware(c, next);
   },
