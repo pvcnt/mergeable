2dab32f4ef1a69678beb68452d6198bcbcb2565c
Vincent Primault <vincent.primault@gmail.com>
2025-05-12 17:42:30 +0200

    feat: restore Neon adapter for Prisma (#160)

--- apps/backend/src/db.ts
+++ apps/backend/src/db.ts
@@ -1,18 +1,15 @@
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../generated/prisma";
 import { type Env } from "./env";
+import { PrismaNeon } from "@prisma/adapter-neon";
 
 export function getPrismaClient(env: Env): PrismaClient {
-  // TODO: disabled because of https://github.com/prisma/prisma/issues/25799
-  /*let adapter: PrismaNeon | PrismaPg;
+  let adapter: PrismaNeon | PrismaPg;
   if (env.DATABASE_URL.indexOf("neon.tech") > -1) {
-    neonConfig.webSocketConstructor = ws;
-    neonConfig.poolQueryViaFetch = true;
     adapter = new PrismaNeon({ connectionString: `${env.DATABASE_URL}` });
   } else {
     adapter = new PrismaPg({ connectionString: `${env.DATABASE_URL}` });
-  }*/
-  const adapter = new PrismaPg({ connectionString: `${env.DATABASE_URL}` });
+  }
   return new PrismaClient({ adapter });
 }
 
