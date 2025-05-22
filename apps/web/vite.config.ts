ef7dc2a731a60ba26ca9896909607ee198151a44
Vincent Primault <vincent.primault@gmail.com>
2025-05-22 13:59:34 +0200

    remove vite plugin

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,20 +1,12 @@
 import { defineConfig } from "vite";
 import { reactRouter } from "@react-router/dev/vite";
 import { codecovVitePlugin } from "@codecov/vite-plugin";
-import processEnv from "@repo/vite-plugin-process-env";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     reactRouter(),
-    codecovVitePlugin({
-      enableBundleAnalysis: process.env.CODECOV_TOKEN !== undefined,
-      bundleName: "webapp",
-      uploadToken: process.env.CODECOV_TOKEN,
-    }),
+    codecovVitePlugin({ uploadToken: process.env.CODECOV_TOKEN }),
   ],
-  worker: {
-    plugins: () => [processEnv()],
-  },
   envPrefix: ["VITE_", "MERGEABLE_"],
 });
