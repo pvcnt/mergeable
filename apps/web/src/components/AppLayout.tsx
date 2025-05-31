cfd9e3e7e039a47edcfdef9bdc5ff8f9cb757877
Vincent <vincent.primault@gmail.com>
2025-05-31 16:03:27 +0200

    fix: login via token in URL from root route

--- apps/web/src/components/AppLayout.tsx
+++ apps/web/src/components/AppLayout.tsx
@@ -1,44 +1,23 @@
-import { Link, Outlet, redirect } from "react-router";
+import { Link, Outlet } from "react-router";
 import clsx from "clsx";
 import Sidebar from "./Sidebar";
 import Footer from "./Footer";
 import styles from "./AppLayout.module.scss";
 import { useLocalStorage } from "usehooks-ts";
-import { deleteConnections, saveConnection } from "../lib/mutations";
-import { db } from "../lib/db";
-import type { Route } from "./+types/AppLayout";
-import { gitHubClient } from "../github";
-import { useConnections } from "../lib/queries";
+import { deleteConnections } from "../lib/mutations";
 import { Card } from "@blueprintjs/core";
-
-export async function clientLoader({ request }: Route.ClientLoaderArgs) {
-  // If the "token" query parameter if provided, and no connections are defined, then
-  // save a new connection for github.com. This allows to login using an OAuth app.
-  const isConnected = (await db.connections.count()) > 0;
-  const url = new URL(request.url);
-  const token = url.searchParams.get("token");
-  if (token && !isConnected) {
-    const props = {
-      label: "",
-      baseUrl: "https://api.github.com",
-      auth: token,
-      host: "github.com",
-      orgs: [],
-    };
-    const viewer = await gitHubClient.getViewer(props);
-    await saveConnection({ id: "", ...props, viewer });
-    return redirect("/inbox");
-  }
-  return null;
-}
+import { useQueryClient } from "@tanstack/react-query";
+import { useConnections } from "../lib/queries";
 
 export default function AppLayout() {
   const [isDark, setDark] = useLocalStorage("isDark", false, {
     initializeWithValue: false, // For SSR support.
   });
+  const queryClient = useQueryClient();
   const connections = useConnections();
-  const handleLogout = () => {
-    deleteConnections().catch(console.error);
+  const handleLogout = async () => {
+    await deleteConnections();
+    await queryClient.invalidateQueries({ queryKey: ["pulls"] });
   };
   return (
     <div className={clsx(styles.app, isDark && "bp5-dark")}>
--- apps/web/src/routes/home.tsx
+++ apps/web/src/routes/home.tsx
@@ -1,5 +1,25 @@
-import { Navigate } from "react-router";
+import { redirect } from "react-router";
+import { db } from "../lib/db";
+import type { Route } from "./+types/home";
+import { gitHubClient } from "../github";
+import { saveConnection } from "../lib/mutations";
 
-export default function Home() {
-  return <Navigate to="/inbox" />;
+export async function clientLoader({ request }: Route.ClientLoaderArgs) {
+  // If the "token" query parameter if provided, and no connections are defined, then
+  // save a new connection for github.com. This allows to login using an OAuth app.
+  const isConnected = (await db.connections.count()) > 0;
+  const url = new URL(request.url);
+  const token = url.searchParams.get("token");
+  if (token && !isConnected) {
+    const props = {
+      label: "",
+      baseUrl: "https://api.github.com",
+      auth: token,
+      host: "github.com",
+      orgs: [],
+    };
+    const viewer = await gitHubClient.getViewer(props);
+    await saveConnection({ id: "", ...props, viewer });
+  }
+  return redirect("/inbox");
 }
